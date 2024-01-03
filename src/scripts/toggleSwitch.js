bb62902656c410c6d45c52db36f9cfeb413b38ed
Trusha Shah <trushashah1402@gmail.com>
2024-01-03 16:21:33 +0530

    Add functionality

---
+++ src/scripts/toggleSwitch.js
@@ -0,0 +1,59 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const textBox = document.getElementById('text-box');
+
+function toggleDarkLightMode(isDark){
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+   isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') :     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+
+
+    nav.style.backgroundColor =isDark ? 'rgb(0 0 0 / 40%)' : 'rgb(255 255 255 / 90%)';
+    textBox.style.backgroundColor =isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)' ;
+}
+
+// // Dark Mode Styles
+// function darkMode() {
+//     toggleIcon.children[0].textContent = 'Dark Mode';
+//     toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+
+//     nav.style.backgroundColor = 'rgb(0 0 0 / 40%)';
+//     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+
+// }
+
+// // Light Mode Styles
+// function lightMode() {
+//     toggleIcon.children[0].textContent = 'Light Mode';
+//     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+//     nav.style.backgroundColor = 'rgb(255 255 255 / 90%)';
+//     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+
+// }
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleDarkLightMode(true);
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleDarkLightMode(false);
+    }
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+// // Check Local Storage For Theme
+// const currentTheme = localStorage.getItem('theme');
+// if (currentTheme) {
+//     document.documentElement.setAttribute('data-theme', currentTheme);
+
+//     if (currentTheme === 'dark') {
+//         toggleSwitch.checked = true;
+//         toggleDarkLightMode(true);
+//     }
+// }
