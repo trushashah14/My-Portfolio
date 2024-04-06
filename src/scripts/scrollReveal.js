31b0acad9a9cb673718311c80c61e02175c989bf
Trusha Shah <trushashah1402@gmail.com>
2024-04-06 23:38:05 +0530

    Changes made in scrollReveal functionality

--- src/scripts/scrollReveal.js
+++ src/scripts/scrollReveal.js
@@ -2,10 +2,9 @@ export default function initScrollReveal(targetElements, defaultProps) {
   if (!targetElements.length) return;
 
   ScrollReveal({ reset: false });
-  const customAnimation = { delay: 40, duration: 600 };
+
 
   targetElements.forEach(({ element, animation }) => {
-    const mergedAnimation = Object.assign({}, defaultProps, customAnimation, animation);
-    ScrollReveal().reveal(element, mergedAnimation);
+    ScrollReveal().reveal(element, Object.assign({}, defaultProps, animation));
   });
 }
